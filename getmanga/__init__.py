a0a40bf2060e92452bc3aedf8e853a67da8575a1
Jamaludin Ahmad <j.ahmad@gmx.net>
2017-12-26 07:53:01 +0700

    remove pretentiousness: rename uri to url.

--- getmanga/__init__.py
+++ getmanga/__init__.py
@@ -16,8 +16,8 @@ import requests
 from lxml import html
 
 
-Chapter = namedtuple('Chapter', 'number name uri')
-Page = namedtuple('Page', 'name uri')
+Chapter = namedtuple('Chapter', 'number name url')
+Page = namedtuple('Page', 'name url')
 
 
 class MangaException(Exception):
@@ -68,7 +68,7 @@ class GetManga(object):
 
         sys.stdout.write("downloading {0} {1}:\n".format(self.title, chapter.number))
 
-        pages = self.manga.get_pages(chapter.uri)
+        pages = self.manga.get_pages(chapter.url)
         progress(0, len(pages))
 
         threads = []
@@ -100,15 +100,15 @@ class GetManga(object):
         """Downloads page images inside a thread"""
         try:
             semaphore.acquire()
-            uri = self.manga.get_image_uri(page.uri)
+            url = self.manga.get_image_url(page.url)
             # mangahere & mangatown have token as trailing query on it's image url
-            query = uri.find('?')
+            query = url.find('?')
             if query != -1:
-                image_ext = uri[:query].split('.')[-1]
+                image_ext = url[:query].split('.')[-1]
             else:
-                image_ext = uri.split('.')[-1]
+                image_ext = url.split('.')[-1]
             name = page.name + os.path.extsep + image_ext
-            image = self.manga.download(uri)
+            image = self.manga.download(url)
         except MangaException as msg:
             queue.put((None, msg))
         else:
@@ -118,7 +118,7 @@ class GetManga(object):
 
 
 class MangaSite(object):
-    site_uri = None
+    site_url = None
     # all but mangareader uses descending chapter list
     descending_list = True
 
@@ -139,16 +139,16 @@ class MangaSite(object):
         return re.sub(r'[^a-z0-9]+', '_', re.sub(r'^[^a-z0-9]+|[^a-z0-9]+$', '', self.input_title))
 
     @property
-    def title_uri(self):
+    def title_url(self):
         """Returns the index page's url of manga title"""
         # this is the most common url for manga title
         # used by: mangafox, mangastream, mangahere, mangatown
-        return "{0}/manga/{1}/".format(self.site_uri, self.title)
+        return "{0}/manga/{1}/".format(self.site_url, self.title)
 
     @property
     def chapters(self):
         """Returns available chapters"""
-        content = self.session.get(self.title_uri).text
+        content = self.session.get(self.title_url).text
         doc = html.fromstring(content)
         _chapters = doc.cssselect(self._chapters_css)
         if self.descending_list:
@@ -159,16 +159,16 @@ class MangaSite(object):
             number = self._get_chapter_number(_chapter)
             location = _chapter.get('href')
             name = self._get_chapter_name(str(number), location)
-            uri = self._get_chapter_uri(location)
-            chapters.append(Chapter(number, name, uri))
+            url = self._get_chapter_url(location)
+            chapters.append(Chapter(number, name, url))
 
         if not chapters:
             raise MangaException("There is no chapter available.")
         return chapters
 
-    def get_pages(self, chapter_uri):
+    def get_pages(self, chapter_url):
         """Returns a list of available pages of a chapter"""
-        content = self.session.get(chapter_uri).text
+        content = self.session.get(chapter_url).text
         doc = html.fromstring(content)
         _pages = doc.cssselect(self._pages_css)
         pages = []
@@ -176,26 +176,26 @@ class MangaSite(object):
             name = self._get_page_name(_page.text)
             if not name:
                 continue
-            uri = self._get_page_uri(chapter_uri, name)
-            pages.append(Page(name, uri))
+            url = self._get_page_url(chapter_url, name)
+            pages.append(Page(name, url))
         return pages
 
-    def get_image_uri(self, page_uri):
-        """Returns uri of image from a chapter page"""
-        content = self.session.get(page_uri).text
+    def get_image_url(self, page_url):
+        """Returns url of image from a chapter page"""
+        content = self.session.get(page_url).text
         doc = html.fromstring(content)
-        image_uri = doc.cssselect(self._image_css)[0].get('src')
+        image_url = doc.cssselect(self._image_css)[0].get('src')
         # use http for mangastream's relative url
-        if image_uri.startswith('//'):
-            return "http:{0}".format(image_uri)
-        return image_uri
+        if image_url.startswith('//'):
+            return "http:{0}".format(image_url)
+        return image_url
 
-    def download(self, image_uri):
+    def download(self, image_url):
         content = None
         retry = 0
         while retry < 5:
             try:
-                resp = self.session.get(image_uri)
+                resp = self.session.get(image_url)
                 if str(resp.status_code).startswith('4'):
                     retry = 5
                 elif str(resp.status_code).startswith('5'):
@@ -208,7 +208,7 @@ class MangaSite(object):
             except Exception:
                 retry += 1
         if not content:
-            raise MangaException("Failed to retrieve {0}".format(image_uri))
+            raise MangaException("Failed to retrieve {0}".format(image_url))
         return content
 
     @staticmethod
@@ -222,13 +222,13 @@ class MangaSite(object):
         """Returns the appropriate name for the chapter for achive name"""
         return "{0}_c{1}".format(self.title, number.zfill(3))
 
-    def _get_chapter_uri(self, location):
+    def _get_chapter_url(self, location):
         """Returns absolute url of chapter's page from location"""
         # some sites already use absolute url on their chapter list, some have relative urls.
         if location.startswith('http://'):
             return location
         else:
-            return "{0}{1}".format(self.site_uri, location)
+            return "{0}{1}".format(self.site_url, location)
 
     @staticmethod
     def _get_page_name(page_text):
@@ -239,16 +239,16 @@ class MangaSite(object):
         return page_text
 
     @staticmethod
-    def _get_page_uri(chapter_uri, page_name):
+    def _get_page_url(chapter_url, page_name):
         """Returns manga image page url"""
         # every sites use different format for their urls, this is a sample.
         # used by: mangahere, mangatown
-        return "{0}{1}.html".format(chapter_uri, page_name)
+        return "{0}{1}.html".format(chapter_url, page_name)
 
 
 class MangaHere(MangaSite):
     """class for mangahere site"""
-    site_uri = "http://www.mangahere.cc"
+    site_url = "http://www.mangahere.cc"
 
     _chapters_css = "div.detail_list ul li a"
     _pages_css = "section.readpage_top div.go_page select option"
@@ -257,7 +257,7 @@ class MangaHere(MangaSite):
 
 class MangaTown(MangaSite):
     """class for mangatown site"""
-    site_uri = "http://www.mangatown.com"
+    site_url = "http://www.mangatown.com"
 
     _chapters_css = "div.chapter_content ul.chapter_list li a"
     _pages_css = "div.manga_read_footer div.page_select select option"
@@ -267,7 +267,7 @@ class MangaTown(MangaSite):
 class MangaFox(MangaSite):
     """class for mangafox site"""
     # their slogan should be: "we are not the best, but we are the first"
-    site_uri = "http://mangafox.la"
+    site_url = "http://mangafox.la"
 
     _chapters_css = "a.tips"
     _pages_css = "#top_bar option"
@@ -282,25 +282,25 @@ class MangaFox(MangaSite):
         return page_text
 
     @staticmethod
-    def _get_page_uri(chapter_uri, page_name):
+    def _get_page_url(chapter_url, page_name):
         """Returns manga image page url"""
         # chapter's page already has the first page's name in it.
-        return re.sub(r'[0-9]+.html$', "{0}.html".format(page_name), chapter_uri)
+        return re.sub(r'[0-9]+.html$', "{0}.html".format(page_name), chapter_url)
 
 
 class MangaStream(MangaSite):
     """class for mangastream site"""
     # a real scanlation group, not distro sites like the others here,
     # currently doesn't utilize _get_page_name and override get_pages instead.
-    site_uri = "https://readms.net"
+    site_url = "https://readms.net"
 
     _chapters_css = "td a"
     _pages_css = "div.btn-group ul.dropdown-menu li a"
     _image_css = "img#manga-page"
 
-    def get_pages(self, chapter_uri):
+    def get_pages(self, chapter_url):
         """Returns a list of available pages of a chapter"""
-        content = self.session.get(chapter_uri).text
+        content = self.session.get(chapter_url).text
         doc = html.fromstring(content)
         _pages = doc.cssselect(self._pages_css)
         for _page in _pages:
@@ -313,8 +313,8 @@ class MangaStream(MangaSite):
         pages = []
         for num in range(1, int(last_page) + 1):
             name = str(num)
-            uri = self._get_page_uri(chapter_uri, name)
-            pages.append(Page(name, uri))
+            url = self._get_page_url(chapter_url, name)
+            pages.append(Page(name, url))
         return pages
 
     @staticmethod
@@ -323,14 +323,14 @@ class MangaStream(MangaSite):
         return chapter.text.split(' - ')[0]
 
     @staticmethod
-    def _get_page_uri(chapter_uri, page_name):
+    def _get_page_url(chapter_url, page_name):
         """Returns manga image page url"""
-        return re.sub('[0-9]+$', page_name, chapter_uri)
+        return re.sub('[0-9]+$', page_name, chapter_url)
 
 
 class MangaReader(MangaSite):
     """class for mangareader site"""
-    site_uri = "http://www.mangareader.net"
+    site_url = "http://www.mangareader.net"
     descending_list = False
 
     _chapters_css = "#chapterlist td a"
@@ -343,28 +343,28 @@ class MangaReader(MangaSite):
         return re.sub(r'[^\-a-z0-9]', '', re.sub(r'[ _]', '-', self.input_title))
 
     @property
-    def title_uri(self):
+    def title_url(self):
         """Returns the index page's url of manga title"""
         # some title's page is in the root, others hidden in a random numeric subdirectory,
         # so we need to search the manga list to get the correct url.
         try:
-            content = self.session.get("{0}/alphabetical".format(self.site_uri)).text
+            content = self.session.get("{0}/alphabetical".format(self.site_url)).text
             page = re.findall(r'[0-9]+/' + self.title + '.html', content)[0]
-            uri = "{0}/{1}".format(self.site_uri, page)
+            url = "{0}/{1}".format(self.site_url, page)
         except IndexError:
-            uri = "{0}/{1}".format(self.site_uri, self.title)
-        return uri
+            url = "{0}/{1}".format(self.site_url, self.title)
+        return url
 
     @staticmethod
-    def _get_page_uri(chapter_uri, page_name='1'):
+    def _get_page_url(chapter_url, page_name='1'):
         """Returns manga image page url"""
         # older stuff, the one in numeric subdirectory, typically named "chapter-X.html",
         # while the new stuff only use number.
-        if chapter_uri.endswith('.html'):
-            page = re.sub(r'\-[0-9]+/', "-{0}/".format(page_name), chapter_uri)
-            return "{0}{1}".format(chapter_uri, page)
+        if chapter_url.endswith('.html'):
+            page = re.sub(r'\-[0-9]+/', "-{0}/".format(page_name), chapter_url)
+            return "{0}{1}".format(chapter_url, page)
         else:
-            return "{0}/{1}".format(chapter_uri, page_name)
+            return "{0}/{1}".format(chapter_url, page_name)
 
 
 SITES = dict(mangafox=MangaFox,
